a9696e901e7073b1a1c265b19d7af98c72ec123e
SRKI972 <srdjan72@blic.net>
2016-12-13 12:11:14 +0100

    Add files via upload

---
+++ projekat3/src/projekat3/clasa2.java
@@ -0,0 +1,28 @@
+package projekat3;
+
+public class clasa2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Deklarisanje i definisanje stringa istovremeno
+				String[] ljudi = {"srki s", "pedja p", "mladjo m"};
+				
+				// Kratka forma FOR petlje 
+				// Ovaj dio se radi tek na sljedecim primjerima, za sada ga usvojiti ovakvog
+				for(String ljud: ljudi) {
+					System.out.println(ljud);
+					
+
+	}
+
+				String[] ime = {"predrag","srdjan","mladen"};
+				String[] prezime = {"nagraisalovic","dodig","tosic"};
+				
+				for (int i=0; i<3; i++){
+					System.out.println(ime[i]+" "+prezime[i]);
+				}
+				
+				
+				
+}
+}
---
+++ projekat4/src/projekat4/class4.java
@@ -0,0 +1,31 @@
+package projekat4;
+
+public class class4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 4;
+	    
+        while(value < 22)
+        {
+            System.out.println("Dobitni broj je: " + value);
+            
+            value = value + 2;
+        }
+        
+        
+                	
+        		int value1 =0;
+        	    
+                while(value1 < 101)
+                {
+                    System.out.println("broj je: " + value1);
+                    
+                    value1 = value1 + 1;
+                }
+        
+        
+        
+	}
+
+}
